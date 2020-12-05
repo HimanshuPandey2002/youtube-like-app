d582aeccc0ba2f36de08eca066a774c809230351
Himanshu Pandey <hp200229@gmail.com>
2020-12-05 20:12:54 +0530

    basic code

---
+++ src/components/VideoDetails.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class VideoDetails extends React.Component {
+  render() {
+    return <h1>This is VideoDetails component</h1>;
+  }
+}
+
+export default VideoDetails;
---
+++ src/components/VideoList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class VideoList extends React.Component {
+  render() {
+    return <h1>This is VideoList component</h1>;
+  }
+}
+
+export default VideoList;
